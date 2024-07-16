8ef6a654796b79f8a4965a9930f1878867f3e47a
Santt4na <jhonnsantt4na@gmail.com>
2024-07-16 20:25:29 -0300

    corriginfo BUG

--- Hora de Codar/8_Node_MySQL/09_Preparando_query/db/conn.js
+++ Hora de Codar/8_Node_MySQL/09_Preparando_query/db/conn.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASS,
     database: process.env.DBS
