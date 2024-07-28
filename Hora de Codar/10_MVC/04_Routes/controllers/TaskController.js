d30c21802e05f4dbf058d5b35b0acd1656da8bd4
Santt4na <jhonnsantt4na@gmail.com>
2024-07-28 20:35:10 -0300

    Feature Rotas

--- Hora de Codar/10_MVC/04_Routes/controllers/TaskController.js
+++ Hora de Codar/10_MVC/04_Routes/controllers/TaskController.js
@@ -4,4 +4,8 @@ module.exports = class TaskController {
     static createTask(req, res) {
         res.render('tasks/create')
     }
+
+    static showTasks(req, res) {
+        res.render('tasks/all');
+    }
 }
