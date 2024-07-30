ba99103d489b41e5fd8d2d1dd925050980d1d732
Santt4na <jhonnsantt4na@gmail.com>
2024-07-29 22:50:10 -0300

    Atualizando os dados

---
+++ Hora de Codar/10_MVC/10_Enviado_Dados_Atualizados/controllers/TaskController.js
@@ -0,0 +1,47 @@
+const Task = require('../models/Task');
+
+module.exports = class TaskController {
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+
+    static async createTaskSave(req, res) {
+        // resgatando os dados do front
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+        // poderiamos fazer validações, processar dados
+        await Task.create(task)
+        res.redirect('/tasks');
+    }
+
+    static async removeTask(req, res) {
+        const id = req.body.id
+        await Task.destroy({ where: { id: id } })
+        res.redirect('/tasks')
+    }
+
+    static async updateTask(req, res) {
+        const id = req.params.id
+        const task = await Task.findOne({ where: { id: id }, raw: true })
+        res.render('tasks/edit', { task })
+    }
+
+    static async updateTaskPost(req, res) {
+        const id = req.body.id
+        const task = {
+            title: req.body.title,
+            description: req.body.description
+        }
+        await Task.update(task, { where: { id: id } })
+        res.redirect('/tasks')
+
+    }
+
+    static async showTasks(req, res) {
+        const tasks = await Task.findAll({ raw: true })
+        res.render('tasks/all', { tasks });
+    }
+}
