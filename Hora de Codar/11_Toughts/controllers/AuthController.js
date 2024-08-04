2ec1af01967dbd91334847315da31746d7d09cd4
Santt4na <jhonnsantt4na@gmail.com>
2024-08-03 22:37:17 -0300

    Adicionando as Flash messages

--- Hora de Codar/11_Toughts/controllers/AuthController.js
+++ Hora de Codar/11_Toughts/controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require('../models/User')
+const bcrypt = require('bcrypt');
+
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login');
@@ -5,5 +9,17 @@ module.exports = class AuthController {
 
     static register(req, res) {
         res.render('auth/register');
+    }
+    static async registerPost(req, res) {
+        // Pegadno todos os dados do body
+        const { name, email, password, confirmpassword } = req.body
+        // passwor match validation
+        if (password != confirmpassword) {
+            // Usando flash cards
+            req.flash('message', 'As Senhas não Confere, Tente novamente')
+            res.render('auth/register')
+            
+            return
+        }
     }
 }
