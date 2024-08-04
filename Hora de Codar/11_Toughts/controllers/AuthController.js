b46cbdade4ebed14655048bece3e25ca3d8fdfa2
Santt4na <jhonnsantt4na@gmail.com>
2024-08-03 22:57:59 -0300

    Style do Flash msg

--- Hora de Codar/11_Toughts/controllers/AuthController.js
+++ Hora de Codar/11_Toughts/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User')
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 
@@ -10,15 +10,16 @@ module.exports = class AuthController {
     static register(req, res) {
         res.render('auth/register');
     }
+
     static async registerPost(req, res) {
         // Pegadno todos os dados do body
-        const { name, email, password, confirmpassword } = req.body
-        // passwor match validation
-        if (password != confirmpassword) {
+        const { name, email, password, confirmPassword } = req.body
+        // password match validation
+        if (password != confirmPassword) {
             // Usando flash cards
             req.flash('message', 'As Senhas não Confere, Tente novamente')
             res.render('auth/register')
-            
+
             return
         }
     }
