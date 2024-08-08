322eea80018ac012115d70e1a7807d7e60920536
Santt4na <jhonnsantt4na@gmail.com>
2024-08-07 23:38:24 -0300

    Iniciando com o MongoDB

---
+++ Hora de Codar/12_MongoDB/02_MVC_Mongodb/routes/produtsRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const ProductController = require('../controllers/ProductController')
+
+router.get('/', ProductController.showProducts);
+
+module.exports = router
