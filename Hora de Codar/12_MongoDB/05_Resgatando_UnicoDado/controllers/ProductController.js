13d2e7b05a07bb854122e91ee75ef0d85ac6e854
Santt4na <jhonnsantt4na@gmail.com>
2024-08-09 20:29:45 -0300

    pegando apenas 1 dado

---
+++ Hora de Codar/12_MongoDB/05_Resgatando_UnicoDado/controllers/ProductController.js
@@ -0,0 +1,26 @@
+const Product = require('../models/Product');
+
+module.exports = class ProductController {
+    static async showProducts(req, res) {
+        const products = await Product.getProducts();
+        res.render('products/all', { products })
+    }
+
+    static createProduct(req, res) {
+        res.render('products/create')
+    }
+
+    static createProductPost(req, res) {
+        const { name, image, price, description } = req.body
+        const product = new Product(name, image, price, description)
+        product.save()
+        res.redirect('/products')
+    }
+
+    static async getProduct(req, res) {
+        const id = req.params.id
+        const product = await Product.getProductById(id);
+        res.render('products/product', { product })
+    }
+
+}
