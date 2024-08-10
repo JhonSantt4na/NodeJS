ef89c16bac4c9999feed38fefd9d79c18010848f
Santt4na <jhonnsantt4na@gmail.com>
2024-08-10 19:36:06 -0300

    Removendo Dados

---
+++ Hora de Codar/12_MongoDB/06_Excluindo_Dados/routes/produtsRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const ProductController = require('../controllers/ProductController')
+
+router.get('/create', ProductController.createProduct);
+router.post('/create', ProductController.createProductPost);
+router.post('/remove/:id', ProductController.removeProduct);
+router.get('/:id', ProductController.getProduct);
+router.get('/', ProductController.showProducts);
+
+
+module.exports = router
