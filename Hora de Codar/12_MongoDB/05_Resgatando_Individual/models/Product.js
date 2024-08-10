029cec4a83aca1205e5b652a47e9168b8c4b66cb
Santt4na <jhonnsantt4na@gmail.com>
2024-08-10 19:17:31 -0300

    fixe bug

---
+++ Hora de Codar/12_MongoDB/05_Resgatando_Individual/models/Product.js
@@ -0,0 +1,34 @@
+const conn = require('../db/conn');
+const { ObjectId } = require('mongodb');
+
+class Product {
+
+    constructor(name, image, price, description) {
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.description = description;
+    }
+
+    async save() {
+        const product = await conn.db().collection('products').insertOne({
+            name: this.name,
+            image: this.image,
+            price: this.price,
+            description: this.description
+        });
+        return product;
+    }
+
+    static async getProducts() {
+        const products = await conn.db().collection('products').find().toArray();
+        return products;
+    }
+
+    static async getProductsById(id) {
+        const product = await conn.db().collection('products').findOne({ _id: new ObjectId(id) });
+        return product;
+    }
+}
+
+module.exports = Product;
