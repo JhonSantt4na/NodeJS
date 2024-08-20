09e7fd8bd2c7381db9e468070c876af5a2c09ae5
Santt4na <jhonnsantt4na@gmail.com>
2024-08-20 09:49:58 -0300

    Refatoração

--- Hora de Codar/15_Get_A_Pet/Backend/helpers/get-token.js
+++
@@ -1,8 +0,0 @@
-const getToken = (req) => {
-   // Bearer eyJhbGciOiJIUz... 
-   const authHeader = req.headers.authorization
-   const token = authHeader.split(" ")[1]
-   return token
-}
-
-module.exports = getToken
