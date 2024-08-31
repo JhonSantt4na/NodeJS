9ce0aed68385fa019d896bef84b2b8a2d8c5cd74
Santt4na <jhonnsantt4na@gmail.com>
2024-08-31 11:53:22 -0300

    App Post

---
+++ NodeJs Vct.Lima/9_Express-Project/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hdbs = require('express-handlebars')
+const Sequelize = require('sequelize');
+const path = require('path');
+require('dotenv').config()
+
+const host = process.env.DB_HOST
+const pass = process.env.DB_PASS
+const user = process.env.DB_USER
+const dbs = process.env.DB_DBS
+const app = express()
+
+// Config Express com o Template Engine
+app.engine('handlebars', hdbs.engine({ defaultLayout: 'main' }))
+// Definindo o handlebars como templete em uso
+app.set('view engine', 'handlebars')
+
+// Conexão DB
+const sequelize = new Sequelize(dbs, user, pass, {
+   host: host,
+   dialect: 'mysql'
+})
+
+// Criando Rotas
+app.get('/cadastro', (req, res) => {
+   // Redenrizando o formulario
+   res.render('formulario')
+})
+app.listen(3000, () => {
+   console.log('Servidor rodando em: http://localhost:3000')
+})
