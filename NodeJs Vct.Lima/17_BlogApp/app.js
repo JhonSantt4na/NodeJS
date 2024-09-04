21a06c30365f9a5a1a5263742fcb7c1b0e639316
Santt4na <jhonnsantt4na@gmail.com>
2024-09-04 20:02:08 -0300

    Adicionando o Bootstrap

--- NodeJs Vct.Lima/17_BlogApp/app.js
+++ NodeJs Vct.Lima/17_BlogApp/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hdbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const admin = require('./routes/Admin')
+const path = require('path')
 
 const app = express()
 
@@ -11,6 +12,9 @@ app.use(express.json())
 app.use(express.urlencoded({
    extended: true
 }))
+// Public
+app.use(express.static(path.join(__dirname, "public")))
+app.set('views', path.join(__dirname, 'views'));
 // handlebars
 app.engine('handlebars', hdbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
--- NodeJs Vct.Lima/17_BlogApp/routes/Admin.js
+++ NodeJs Vct.Lima/17_BlogApp/routes/Admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-   res.send('Pagina Principal do ADM')
+   res.render('admin/index')
 })
 
 router.get('/posts', (req, res) => {
