8dd2ec182cdf819ed744c6b78159af001ed54dba
Santt4na <jhonnsantt4na@gmail.com>
2024-09-07 23:17:42 -0300

    Editando as Categorias

--- NodeJs Vct.Lima/17_BlogApp/routes/Admin.js
+++ NodeJs Vct.Lima/17_BlogApp/routes/Admin.js
@@ -69,4 +69,36 @@ router.get('/categorias/add', (req, res) => {
    res.render('admin/addcategorias');
 });
 
+router.get('/categorias/edit/:id', (req, res) => {
+   Categoria.findOne({ _id: req.params.id }).lean()
+      .then((categoria) => {
+         res.render('admin/editcategorias', { categoria });
+      }).catch((err) => {
+         console.log(err)
+         req.flash("error_msg", "Essa Categorias não existe, Tente Novamente!");
+         res.redirect('/admin/categorias');
+      })
+
+})
+
+router.post('/categorias/edit', (req, res) => {
+   Categoria.findOne({ _id: req.body.id })
+      .then((categoria) => {
+         categoria.nome = req.body.nome
+         categoria.slug = req.body.slug
+         categoria.save().then(() => {
+            req.flash('success_msg', 'Categoria editada com sucesso!')
+            res.redirect('/admin/categorias')
+         }).catch((err) => {
+            req.flash('error_msg', "Houve um erro ao salvar a edição da categoria")
+            res.redirect('/admin/categorias')
+         })
+      })
+      .catch((err) => {
+         console.log(err)
+         req.flash("error_msg", "Houve um erro ao Editar a Categoria, Tente Novamente!");
+         res.redirect('/admin/categorias');
+      })
+})
+
 module.exports = router;
