22ab7e9e86c843391565be734d3f0b5b0fbabf56
Santt4na <jhonnsantt4na@gmail.com>
2024-09-10 19:10:31 -0300

    Deletando postagem de uma forma não convencional

--- NodeJs Vct.Lima/17_BlogApp/routes/Admin.js
+++ NodeJs Vct.Lima/17_BlogApp/routes/Admin.js
@@ -210,4 +210,14 @@ router.post('/postagens/edit', (req, res) => {
       })
 })
 
+router.get('/postagens/deletar/:id', (req, res) => {
+   Postagem.deleteOne({ _id: req.params.id }).then(() => {
+      req.flash('success_msg', "Postagem deletada com sucesso!")
+      res.redirect('/admin/postagens')
+   }).catch((err) => {
+      req.flash("error_msg", "Houve um erro interno")
+      res.redirect('/admin/postagens')
+   })
+})
+
 module.exports = router;
